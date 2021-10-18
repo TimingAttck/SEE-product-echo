40a6f79d37055e5e3aa2c464acacd6f0df3e1841
TimingAttck <ohlhoffclaude@hotmail.com>
2021-10-19 01:01:54 +0200

    Making the spring boot project tomcat compatible

--- src/main/java/lu/claude/ohlhoff/echo/EchoApplication.java
+++ src/main/java/lu/claude/ohlhoff/echo/EchoApplication.java
@@ -2,9 +2,12 @@ package lu.claude.ohlhoff.echo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class EchoApplication {
+@ComponentScan(basePackages="lu.claude.ohlhoff.echo")
+public class EchoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchoApplication.class, args);
